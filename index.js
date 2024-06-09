3a25c7bba6744dc6ccc1235afc3725262bb25731
Legion204 <ri46801@gmail.com>
2024-06-09 22:21:53 +0600

    added jwt security in server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -8,8 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 
 
@@ -34,6 +33,40 @@ async function run() {
     const userCollection = client.db("newsDB").collection("users");
     const publicationCollection = client.db("newsDB").collection("publications");
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' });
+      res.send({ token });
+    });
+
+    // Custom middlewares 
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
+      next();
+    }
+
     // user related api
     app.post('/users', async (req, res) => {
       const user = req.body
@@ -48,7 +81,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
@@ -65,6 +98,20 @@ async function run() {
       res.send(result)
     });
 
+    app.get("/users/admin", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    });
+
     // article related api
     app.post("/articles", async (req, res) => {
       const data = req.body
@@ -72,26 +119,26 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/articles", async (req, res) => {
+    app.get("/articles", verifyToken, async (req, res) => {
       const query = { status: "approved", isPremium: false }
       const result = await articleCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.get("/article/:id", async (req, res) => {
+    app.get("/article/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await articleCollection.findOne(query)
       res.send(result);
     });
 
-    app.get("/premium_articles", async (req, res) => {
+    app.get("/premium_articles", verifyToken, async (req, res) => {
       const query = { isPremium: true, status: "approved" }
       const result = await articleCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.get("/my_articles", async (req, res) => {
+    app.get("/my_articles", verifyToken, async (req, res) => {
       const email = req.query.email
       const query = { authorEmail: email }
       const result = await articleCollection.find(query).toArray();
@@ -127,7 +174,7 @@ async function run() {
     });
 
     // api for admin
-    app.get("/articles/admin", async (req, res) => {
+    app.get("/articles/admin", verifyToken, async (req, res) => {
       const result = await articleCollection.find().toArray();
       res.send(result);
     });
@@ -177,7 +224,7 @@ async function run() {
       res.send(result)
     });
 
-    app.get("/publications", async (req, res) => {
+    app.get("/publications", verifyToken, async (req, res) => {
       const result = await publicationCollection.find().toArray()
       res.send(result)
     });
